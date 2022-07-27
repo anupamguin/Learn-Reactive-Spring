68132d66c3d26187168801662da7bb16c0c4c9fa
Anupam <anupam@etranssolutions.com>
2022-07-27 18:54:06 +0530

    add reactive test case

---
+++ src/test/java/com/learn/reactive/spring/fluxmonoplayground/FluxTest.java
@@ -0,0 +1,43 @@
+package com.learn.reactive.spring.fluxmonoplayground;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxTest {
+
+	@Test
+	public void fluxTestElements_WithError() {
+		
+		Flux<String> firstFlux = Flux.just("West Bengal","Odisha","Delhi").log();
+		
+		StepVerifier.create(firstFlux)
+		   .expectNext("West Bengal")
+		   .expectNext("Odisha","Delhi")
+		   .verifyComplete();
+		
+		System.err.println("-----------------------------------------------------------");
+		
+		
+		Flux<String> secondFlux = Flux.just("one","two","three")
+				.concatWith(Flux.error(new RuntimeException("Exception Created ..")))
+				.log();
+		StepVerifier.create(secondFlux)
+			.expectNext("one")
+			.expectNext("two")
+			.expectNext("three")
+			.expectError(RuntimeException.class)
+			.verify();
+		
+		System.err.println("-----------------------------------------------------------");
+		
+		Flux<String> threeFlux = Flux.just("four","five","six")
+				.concatWith(Flux.error(new RuntimeException("Exception Created ..")))
+				.log();
+		StepVerifier.create(threeFlux)
+		     .expectNextCount(3)
+		     .expectErrorMessage("Exception Created ..")
+		     .verify();
+	}
+}
